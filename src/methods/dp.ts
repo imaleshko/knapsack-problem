20a0b7b01bfc0e15fbf552b43191cff1445c00be
ivan_maleshko <maleshko.ivan@student.uzhnu.edu.ua>
2026-04-29 14:31:42 +0300

    feat: dynamic programming method

---
+++ src/methods/dp.ts
@@ -0,0 +1,48 @@
+import type { Item, KnapsackInput } from "@/interfaces.ts";
+
+export interface DPResult {
+  table: number[][];
+  maxValue: number;
+  selectedItems: Item[];
+}
+
+export const solveDP = ({ items, capacity }: KnapsackInput): DPResult => {
+  const n = items.length;
+
+  const table: number[][] = Array(n + 1)
+    .fill(0)
+    .map(() => Array(capacity + 1).fill(0));
+
+  for (let i = 1; i <= n; i++) {
+    const currentItem = items[i - 1];
+
+    for (let w = 1; w <= capacity; w++) {
+      if (currentItem.weight <= w) {
+        table[i][w] = Math.max(
+          table[i - 1][w],
+          table[i - 1][w - currentItem.weight] + currentItem.value,
+        );
+      } else {
+        table[i][w] = table[i - 1][w];
+      }
+    }
+  }
+
+  const selectedItems: Item[] = [];
+  let currentCapacity = capacity;
+  for (let i = n; i > 0; i--) {
+    if (table[i][currentCapacity] !== table[i - 1][currentCapacity]) {
+      const includedItem = items[i - 1];
+      selectedItems.push(includedItem);
+      currentCapacity -= includedItem.weight;
+    }
+  }
+
+  selectedItems.reverse();
+
+  return {
+    table: table,
+    maxValue: table[n][capacity],
+    selectedItems,
+  };
+};
